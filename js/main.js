c176a0d2d0b1d93797fd5cabded2e2099bbb30c3
pukun1115-tech <pukun1115@gmail.com>
2026-09-06 22:05:50 +0900

    resizeCanvas関数

--- js/main.js
+++ js/main.js
@@ -1,13 +1,41 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 300;
-canvas.height = 600;
+function resizeCanvas() {
+    const w = Math.floor(window.innerWidth);
+    const h = Math.floor(window.innerHeight / 2) * 2;
+
+    //画面上の表示サイズ
+    let displayWidth, displayHeight;
+
+    if ((h / 2) > w) {
+        displayWidth = w;
+        displayHeight = w * 2;
+    } else {
+        displayWidth = h / 2;
+        displayHeight = h;
+    }
+
+    canvas.style.width = displayWidth + "px";
+    canvas.style.height = displayHeight + "px";
+
+    const dpr = window.devicePixelRatio || 1;
+
+    canvas.width = displayWidth * dpr;
+    canvas.height = displayHeight * dpr;
+
+    ctx.scale(dpr, dpr);
+}
+
+window.addEventListener("resize", resizeCanvas);
 
 const ohajikiArray = [];
 
 function mainLoop() {
-    ctx.crearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    requestA
+    requestAnimationFrame(mainLoop);
 }
+
+resizeCanvas();
+mainLoop();
