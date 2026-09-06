71cd6b3fa04502eacb7d521ebf4810098b380191
pukun1115-tech <pukun1115@gmail.com>
2026-09-06 19:13:10 +0900

    おはじき

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 300;
+canvas.height = 600;
+
+const ohajikiArray = [];
+
+function mainLoop() {
+    ctx.crearRect(0, 0, canvas.width, canvas.height);
+
+    requestA
+}
---
+++ js/ohajiki.js
@@ -0,0 +1,9 @@
+class ohajiki {
+    constructor(x, y, radius, weight, color ) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.weight = weight;
+        this.color = color;
+    }
+}
