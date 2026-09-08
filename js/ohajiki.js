ef1b8dbadfb0e62ad044209eb38f7e18d99d7fcd
pukun1115-tech <pukun1115@gmail.com>
2026-09-08 13:47:58 +0900

    引っ張ってバーン

--- js/ohajiki.js
+++ js/ohajiki.js
@@ -6,6 +6,12 @@ class ohajiki {
         this.radius = radius;
         this.weight = weight;
         this.color = color;
+        //飛ばす方向描画用
+        this.tx = null;
+        this.ty = null;
+        //飛ぶ方向
+        this.gx = 0;
+        this.gy = 0;
     }
     
     draw(canvas, ctx) {
@@ -13,5 +19,10 @@ class ohajiki {
         ctx.beginPath();
         ctx.arc(canvas.width * this.x, canvas.width * this.y, canvas.width * this.radius, 0, Math.PI * 2);
         ctx.fill();
+    }
+
+    update() {
+        this.x += this.gx;
+        this.y += this.gy;
     }
 }
